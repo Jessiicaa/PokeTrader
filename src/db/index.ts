98d67e85fea2070d772b58005e7096cc551be204
Jessica Oliveira <jessiicaasoliveira@gmail.com>
2021-07-20 20:01:24 -0300

    Create db

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import { connect, connection } from 'mongoose';
+import { env } from 'process';
+
+export default () => {
+  const connectionDB = () => {
+    connect(env.URI_DB ? env.URI_DB : 'mongodb://localhost:27017/pokeTrade',
+      {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        user: env.USER_DB ? env.USER_DB : 'pokemon',
+        pass: env.USER_PWD_DB ? env.USER_PWD_DB : 'pokemonPwd',
+        authSource: 'admin',
+      }).catch((error: any) => (console.error('Crashed => ', error)));
+  };
+  connectionDB();
+  connection.on('error', () => (console.error('🤬  Not possible connect on MongoDb.')));
+};
