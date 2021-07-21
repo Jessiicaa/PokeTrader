1a0f9eac061c6c9cab1d5da4a33f9bc9a88bc930
Jessica Oliveira <jessiicaasoliveira@gmail.com>
2021-07-20 21:03:54 -0300

    Update port

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { env } from 'process';
 import routes from './routes';
 
 const app = express();
@@ -7,6 +8,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log(`Server started on port ${3333}!`);
+app.listen(env.PORT, () => {
+  console.log(`Server started on port ${env.PORT}!`);
 });
